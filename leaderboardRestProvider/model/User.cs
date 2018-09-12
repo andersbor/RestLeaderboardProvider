2ba6c7a9feee3a381c543eed601cf5bdb6913393
andersbor <anbo@easj.dk>
2018-09-12 10:28:14 +0200

    initial

--- leaderboardRestProvider/model/User.cs
+++ leaderboardRestProvider/model/User.cs
@@ -4,7 +4,7 @@ namespace leaderboardRestProvider.model
 {
     public class User
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Username { get; set; }
         public string PasswordHashed { get; set; }
         public DateTime Enrolled { get; set; }
